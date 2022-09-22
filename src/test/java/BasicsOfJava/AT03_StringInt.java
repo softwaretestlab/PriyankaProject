6026b11b1d7b3f2166067b1772d34c972e6c2ed1
softwaretestlab <softwaretestlab@hotmail.com>
2022-09-22 11:03:57 +1000

    new

---
+++ src/test/java/BasicsOfJava/AT03_StringInt.java
@@ -0,0 +1,27 @@
+package BasicsOfJava;
+
+import org.junit.jupiter.api.Test;
+
+public class AT03_StringInt {
+
+    @Test
+    public void test03(){
+        //declare local variables
+        int i=10;
+        int j=20;
+        int k = i+j; //op-->30
+        System.out.println("Value of K is : "+k);
+
+    }
+
+    @Test
+    public void test04(){
+        //declare local variables
+        String test01 ="Software";
+        String test02 ="testlab";
+        String test03 = test01 + test02;
+        System.out.println("value of Test03 is : " + test03);
+
+    }
+
+}
